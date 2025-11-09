8829be25d47bc717f3fe7f94fc2197f05d403c77
Ian Beckett <ianedwardbeckett@gmail.com>
2025-11-08 19:55:32 -0600

    chore(nvim): format autocommands file

--- env/.config/nvim/lua/autocommands.lua
+++ env/.config/nvim/lua/autocommands.lua
@@ -11,20 +11,19 @@ vim.api.nvim_create_autocmd("FileType", {
 -- When I press o in normal mode, I don't get a new commented line.
 
 vim.api.nvim_create_autocmd('FileType', {
-    group = vim.api.nvim_create_augroup('mariasolos/big_file', { clear = true }),
-    desc = 'Disable features in big files',
-    pattern = 'bigfile',
-    callback = function(args)
-        vim.schedule(function()
-            vim.bo[args.buf].syntax = vim.filetype.match { buf = args.buf } or ''
-        end)
-    end,
+  group = vim.api.nvim_create_augroup('mariasolos/big_file', { clear = true }),
+  desc = 'Disable features in big files',
+  pattern = 'bigfile',
+  callback = function(args)
+    vim.schedule(function()
+      vim.bo[args.buf].syntax = vim.filetype.match { buf = args.buf } or ''
+    end)
+  end,
 })
 
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
   callback = function(event)
-
     local map = function(keys, func, desc, mode) --TODO: why use this?
       mode = mode or 'n'
       vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
@@ -41,15 +40,15 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 
 vim.api.nvim_create_autocmd('BufReadPost', {
-    group = vim.api.nvim_create_augroup('mariasolos/last_location', { clear = true }),
-    desc = 'Go to the last location when opening a buffer',
-    callback = function(args)
-        local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
-        local line_count = vim.api.nvim_buf_line_count(args.buf)
-        if mark[1] > 0 and mark[1] <= line_count then
-            vim.cmd 'normal! g`"zz'
-        end
-    end,
+  group = vim.api.nvim_create_augroup('mariasolos/last_location', { clear = true }),
+  desc = 'Go to the last location when opening a buffer',
+  callback = function(args)
+    local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+    local line_count = vim.api.nvim_buf_line_count(args.buf)
+    if mark[1] > 0 and mark[1] <= line_count then
+      vim.cmd 'normal! g`"zz'
+    end
+  end,
 })
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "netrw",
