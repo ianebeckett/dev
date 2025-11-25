61b108e115a398686efad183de5e22433429f74e
Ian Beckett <ianedwardbeckett@gmail.com>
2025-11-25 09:06:58 -0600

    harpoon and trouble remaps

--- env/.config/nvim/after/plugin/harpoon/harpoon.lua
+++ env/.config/nvim/after/plugin/harpoon/harpoon.lua
@@ -8,8 +8,10 @@ harpoon.settings = {
   end,
 }
 
-vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end)
 vim.keymap.set('n', '<C-e>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end)
+vim.keymap.set('n', '<leader>A', function() harpoon:list():add() end)
+-- remap these to C+1243 once I figure out how to mamke it work with tmux
 vim.keymap.set('n', '<A-h>', function() harpoon:list():select(1) end)
 vim.keymap.set('n', '<A-j>', function() harpoon:list():select(2) end)
 vim.keymap.set('n', '<A-k>', function() harpoon:list():select(3) end)
--- env/.config/nvim/after/plugin/trouble/trouble.lua
+++ env/.config/nvim/after/plugin/trouble/trouble.lua
@@ -1,10 +1,10 @@
 local trouble = require('trouble')
 local opts = { skip_groups = true, jump = true }
 trouble.setup({
-  config = {
-    icons = false,
+    config = {
+        icons = false,
+    },
     vim.keymap.set("n", "<leader>tt", function() trouble.toggle('diagnostics') end),
-    vim.keymap.set("n", "[t", function() trouble.next(opts); end),
-    vim.keymap.set("n", "]t", function() trouble.previous(opts); end),
-  },
+    vim.keymap.set("n", "tn", function() trouble.next(opts); end),
+    vim.keymap.set("n", "tp", function() trouble.previous(opts); end),
 })
