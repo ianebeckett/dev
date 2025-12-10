5f55bdcaa923ca91a84c0e9fb4ae5ed4c0afc5e5
Ian Beckett <ianedwardbeckett@gmail.com>
2025-12-09 23:08:57 -0600

    chore(nvim): rearrange nvim-treesitter config

--- env/.config/nvim/after/plugin/treesitter/treesitter.lua
+++ env/.config/nvim/after/plugin/treesitter/treesitter.lua
@@ -23,25 +23,19 @@ require 'nvim-treesitter.configs'.setup {
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
 
+  -- List of parsers to ignore installing (or 'all')
+  -- ignore_install = { 'javascript' },
+
   highlight = {
     enable = true,
 
-    -- List of parsers to ignore installing (or 'all')
-    -- ignore_install = { 'javascript' },
-
-    -- list of language that will be disabled
-    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
-    -- disable = { 'c', 'rust', 'latex' },
-
-    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+    --use this for didsabling in big files?
     disable = function(lang, buf)
-      local max_filesize = 100 * 1024 -- 100 KB
-      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-      if ok and stats and stats.size > max_filesize then
-        return true
-      end
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+            return true
+        end
     end,
 
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
