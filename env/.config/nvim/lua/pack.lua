685fcb63061a0935dd9125f779569d82fde93944
Ian Beckett <ianedwardbeckett@gmail.com>
2025-12-14 21:15:44 -0600

    update packages. Pin nvim-treesitter to master

--- env/.config/nvim/lua/pack.lua
+++ env/.config/nvim/lua/pack.lua
@@ -3,24 +3,30 @@
 -- delete a package by name
 -- lua vim.pack.del({<name>})
 -- it will reinstall unless it's been removed from vim.pack.add()
-vim.pack.add({
-    --colorschemes
-    'https://github.com/rose-pine/neovim.git',
-    'https://github.com/lewis6991/gitsigns.nvim.git',
-    'https://github.com/nvim-treesitter/nvim-treesitter.git',
-    'https://github.com/nvim-lua/plenary.nvim.git',
-    { --depends on plenary, ripgrep, fd
-        src = 'https://github.com/nvim-telescope/telescope.nvim.git',
-        version = 'release',
-    },
-    { --depends on plenary
-        src = 'https://github.com/ThePrimeagen/harpoon',
-        version = 'harpoon2',
-    },
-    'https://github.com/saghen/blink.cmp.git',
+vim.pack.add(
     {
-        src = 'https://github.com/L3MON4D3/LuaSnip.git',
-        version = 'v2.*',
+        --colorschemes
+        'https://github.com/rose-pine/neovim.git',
+        'https://github.com/lewis6991/gitsigns.nvim.git',
+        {
+            src = 'https://github.com/nvim-treesitter/nvim-treesitter.git',
+            version = 'master',
+        },
+        'https://github.com/nvim-lua/plenary.nvim.git',
+        { --depends on plenary, ripgrep, fd
+            src = 'https://github.com/nvim-telescope/telescope.nvim.git',
+            version = vim.version.range('^0.2.0'),
+        },
+        { --depends on plenary
+            src = 'https://github.com/ThePrimeagen/harpoon',
+            version = 'harpoon2',
+        },
+        'https://github.com/saghen/blink.cmp.git',
+        {
+            src = 'https://github.com/L3MON4D3/LuaSnip.git',
+            version = vim.version.range( '^2.4.1'),
+        },
+        'https://github.com/rafamadriz/friendly-snippets',
     },
-    'https://github.com/rafamadriz/friendly-snippets',
-})
+    { confirm = false }
+)
