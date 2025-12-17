030c4cafc3356a73818e1ffaa1a1f82418652d60
Ian Beckett <ianedwardbeckett@gmail.com>
2025-12-17 04:10:32 -0600

    feat(nvim): make clangd ask gcc for includepaths. more root markers

--- env/.config/nvim/lsp/clangd.lua
+++ env/.config/nvim/lsp/clangd.lua
@@ -1,5 +1,18 @@
 return {
-  cmd = { 'clangd', '--clang-tidy', '--background-index' },
-  root_markers = { { 'compile_commands.json', 'compile_flags.txt' }, },
-  filetypes = { 'c', 'cpp', 'h', 'hpp' },
+    cmd = {
+        'clangd',
+        '--clang-tidy',
+        '--background-index',
+        "--query-driver=/usr/bin/g++"
+    },
+    root_markers = {{
+        '.clangd',
+        '.clang-tidy',
+        '.clang-format',
+        'compile_commands.json',
+        'compile_flags.txt',
+        'configure.ac',
+        '.git'
+    }},
+    filetypes = { 'c', 'cpp', 'h', 'hpp' },
 }
