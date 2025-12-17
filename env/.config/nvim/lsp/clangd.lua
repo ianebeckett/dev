5afa2dd603e64c65b0140697f443079d7768c3bb
Ian Beckett <ianedwardbeckett@gmail.com>
2025-12-17 05:13:20 -0600

    comment for clangd

--- env/.config/nvim/lsp/clangd.lua
+++ env/.config/nvim/lsp/clangd.lua
@@ -1,3 +1,21 @@
+-- compile.commands.json is needed in the project root. Something like this:
+-- [
+--   {
+--     "arguments": [
+--       "/usr/bin/g++",
+--       "-c",
+--       "-Wall",
+--       "-Wextra",
+--       "-Wpedantic",
+--       "a.cpp"
+--     ],
+--     "directory": "/home/ian/school/undergrad/430_strategy/lab01",
+--     "file": "/home/ian/school/undergrad/430_strategy/lab01/a.cpp"
+--   }
+-- ]
+--
+-- We can make it with bear -- g++ -foo -bar a.cpp
+
 return {
     cmd = {
         'clangd',
