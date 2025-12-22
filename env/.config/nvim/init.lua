e0a00f5d7edde885d8bcb061844f94cee4eafc1c
Ian Beckett <ianedwardbeckett@gmail.com>
2025-12-21 20:41:42 -0600

    nvim: remove kanagawa, add notes on colorschemes

--- env/.config/nvim/init.lua
+++ env/.config/nvim/init.lua
@@ -4,8 +4,13 @@ require('autocommands')
 require('options')
 require('keymaps')
 
+--notes on colorschemes I've tried:
+--gruvbox-material
+--gruvbox-baby
+--kanagawa - looks too muted on my screen. Might look better on a glossy display
+--mellifluous - I don't like beige strings
+--nord - floating windows are white on black. Also, the dusty blue bg is not my fave
 require('colors.rose-pine')
---require('colors.kanagawa')
 
 -- Interactive textual undotree
 vim.cmd.packadd 'nvim.undotree'
--- env/.config/nvim/lua/pack.lua
+++ env/.config/nvim/lua/pack.lua
@@ -6,7 +6,6 @@
 vim.pack.add(
     {
         'https://github.com/rose-pine/neovim.git',
-        'https://github.com/rebelot/kanagawa.nvim.git',
         'https://github.com/lewis6991/gitsigns.nvim.git',
         'https://github.com/nvim-treesitter/nvim-treesitter.git',
         {
